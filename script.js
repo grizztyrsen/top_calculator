cd333e918cd4e959deed56507ee6727a0c6ff41a
Zack Norton <160171391+grizztyrsen@users.noreply.github.com>
2024-05-11 12:08:53 -0500

    finished calculator

---
+++ script.js
@@ -0,0 +1,110 @@
+let operator = ""
+let previousValue = ""
+let currentValue = ""
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    //store all components on HTML in JS
+    let clear = document.querySelector(".clear");
+    let decimal = document.querySelector(".decimal")
+    let equal = document.querySelector(".equal")
+
+    let numbers = document.querySelectorAll(".number");
+    let operators = document.querySelectorAll(".operator");
+
+    let previousScreen = document.querySelector(".previous")
+    let currentScreen = document.querySelector(".current")
+
+    numbers.forEach((number) => number.addEventListener("click", function(e){
+        handleNumber(e.target.textContent)
+        currentScreen.textContent = currentValue
+    }))
+
+    operators.forEach((op) => op.addEventListener("click", function(e){
+        handleOperator(e.target.textContent)
+        previousScreen.textContent = `${previousValue} ${operator}`
+        currentScreen.textContent = currentValue
+
+    }))
+
+    clear.addEventListener("dblclick", function(e){
+        previousValue = ''
+        currentValue = ''
+        operator = ''
+        previousScreen.textContent=''
+        currentScreen.textContent = ''
+    })
+
+    clear.addEventListener("click", function(e){
+        currentValue = ''
+        currentScreen.textContent = ''
+    })
+
+    
+
+    equal.addEventListener("click", function(){
+        if(currentValue != '' && previousValue != ''){
+            calculate()
+            previousScreen.textContent = ""
+            currentScreen.textContent = previousValue
+            if(previousValue.length <= 5){
+            currentScreen.textContent = previousValue;
+            } else{
+            currentScreen.textContent = previousValue.slice(0,5) + '...'
+            }
+        }
+        
+    })
+
+    decimal.addEventListener("click", function(){
+        addDecimal()
+
+    })
+
+})
+
+function handleNumber(num){
+    if(currentValue.length <= 5){
+    currentValue += num}
+
+}
+
+function handleOperator(op){
+    operator = op
+    previousValue = currentValue;
+    currentValue= ""
+
+}
+
+function calculate(){
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+
+    if(operator === "+"){
+        previousValue += currentValue;
+    }else if (operator === "-"){
+        previousValue -= currentValue;
+    } else if (operator === "X"){
+        previousValue *= currentValue;
+    } else{
+        previousValue /= currentValue;
+    }
+
+    previousValue = roundNumber(previousValue)
+    previousValue = previousValue.toString()
+    currentValue = previousValue.toString()
+
+}
+
+function roundNumber(num){
+    return Math.round(num * 1000)/1000
+}
+
+function addDecimal(){
+    if(!currentValue.includes(".")){
+        currentValue += "."
+    }
+
+
+}
